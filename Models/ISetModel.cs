6689c9a07c62617dbcba719005be1a3557c5b12b
Nicole Sharabany <nicolesharabani7@gmail.com>
2021-04-09 12:11:12 +0300

    Set MVVM added, binding csvpath not working yet.

---
+++ Models/ISetModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace FlightSimulator.Models
+{
+    interface ISetModel : INotifyPropertyChanged
+    {
+        ArrayList HeadersList { get; set; }
+        string CsvPath { get; set; }
+        Dictionary<string, ArrayList> DataMap { get; set; }
+
+    }
+}
--- ViewModels/SetViewModel.cs
+++ ViewModels/SetViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightSimulator.Models;
 
 namespace FlightSimulator.ViewModels
 {
-    class SetViewModel
+    class SetViewModel : INotifyPropertyChanged
     {
+        private ISetModel model;
+        public SetViewModel(ISetModel m)
+        {
+            this.model = m;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) { NotifyPropertyChanged("VM_" + e.PropertyName); };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        public string VM_CsvPath
+        {
+            get
+            {
+                return model.CsvPath;
+            }
+            set
+            {
+                model.CsvPath = value;
+            }
+        }
+        public Dictionary<string, ArrayList> VM_DataMap
+        {
+            get
+            {
+                return model.DataMap;
+            }
+        }
+
     }
 }
