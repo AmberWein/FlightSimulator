27f92eb56d356e715f2ad213150d1e80517c83d8
Amber Weiner <17.amber@gmail.com>
2021-04-09 09:32:40 +0300

    add File_Parser and CSV_Parser

---
+++ IO/CSV_Parser.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Globalization;
+
+namespace FlightSimulator.IO
+{
+    class CSV_Parser: File_Parser
+    {
+        private ArrayList properties;
+        private ArrayList lines;
+        private int numOfProperties;
+        private Dictionary<string, ArrayList> map;
+        public ArrayList Properties
+        {
+            get
+            {
+                return properties;
+            }
+            set
+            {
+                properties = value;
+            }
+        }
+        public ArrayList Lines
+        {
+            get
+            {
+                return lines;
+            }
+            set
+            {
+                lines = value;
+            }
+        }
+        public int NumOfProperties
+        {
+            get
+            {
+                return numOfProperties;
+            }
+            set
+            {
+                numOfProperties = value;
+            }
+        }
+
+        public Dictionary<string, ArrayList> Map
+        {
+            get
+            {
+                return map;
+            }
+            set
+            {
+                map = value;
+            }
+        }
+        public CSV_Parser(string newFilePath, ArrayList newProperties) : base(newFilePath)
+        {
+            this.properties = newProperties;
+            this.numOfProperties = properties.Count;
+            this.map = new Dictionary<string, ArrayList>();
+            this.lines = new ArrayList();
+        }
+
+
+        public override void Parse()
+        {
+            var reader = new StreamReader(File.OpenRead(filePath));
+            int i = 0;
+
+            while (i < this.numOfProperties)
+            // maybe we should check validity- meaning if the amount of rows matches to the properties size
+            {
+                string line = reader.ReadLine();
+                string[] valuesString = line.Split(',');
+                string key = properties[i].ToString();
+                i++;
+                ArrayList listOfValues = new ArrayList();
+                foreach (string value in valuesString)
+                {
+                    Lines.Add(line);
+                    float courrentValue = float.Parse(value, CultureInfo.InvariantCulture);
+                    listOfValues.Add(courrentValue);
+                }
+                this.map.Add(key, listOfValues);
+            }
+            reader.Dispose();
+        }
+    }
+}
---
+++ IO/File_Parser.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace FlightSimulator.IO
+{
+    abstract class File_Parser
+    {
+        protected string filePath;
+        public string FilePath
+        {
+            get
+            {
+                return filePath;
+            }
+            set
+            {
+                filePath = value;
+            }
+        }
+
+        public File_Parser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public static bool IsValidPath(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+        virtual public void Parse()
+        {
+            // backup validity check
+            if (IsValidPath(this.filePath) == false)
+                return;
+        }
+    }
+}
