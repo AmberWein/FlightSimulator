2a1883a236f1168c21847849735eca14ac27573e
Amber Weiner <17.amber@gmail.com>
2021-04-10 19:50:06 +0300

    fix comboBox binding

--- Views/MediaPlayerView.xaml.cs
+++ Views/MediaPlayerView.xaml.cs
@@ -23,11 +23,28 @@ namespace FlightSimulator.Views
     public partial class Media_Player_View : UserControl
     {
         private MediaPlayerViewModel vm;
+        List<double> speeds;
+
         public Media_Player_View()
         {
+            FillList();
             InitializeComponent();
             vm = new MediaPlayerViewModel(new MediaPlayerModel());
             DataContext = vm;
+            Binding dict = new Binding
+            {
+                Source = speeds
+            };
+            speedBox.SetBinding(ComboBox.ItemsSourceProperty, dict);
+        }
+        private void FillList()
+        {
+            this.speeds = new List<double>();
+
+            for (double i = 1; i <= 2; i += 0.25)
+            {
+                this.speeds.Add(i);
+            }
         }
         private void Open_Media_Player_Click(object sender, RoutedEventArgs e)
         {
@@ -47,25 +64,30 @@ namespace FlightSimulator.Views
         }
         private void Play_Media_Player_Click(object sender, RoutedEventArgs e)
         {
-            this.vm.VM_IsPlay = true;
+            this.vm.VM_Timer -= 0.1;
         }
         private void Stop_Media_Player_Click(object sender, RoutedEventArgs e)
         {
-            this.vm.VM_Timer = 0;
-            this.vm.VM_IsPlay = false;
+            if(this.vm.VM_Timer != 0)
+                this.vm.VM_Timer = 0;
+
+            if (this.vm.VM_Speed != 0)
+                this.vm.VM_Speed = 0;
         }
         private void Pause_Media_Player_Click(object sender, RoutedEventArgs e)
         {
-            this.vm.VM_IsPlay = false;
+            if (this.vm.VM_Speed != 0)
+                this.vm.VM_Speed = 0;
         }
         private void Forward_Media_Player_Click(object sender, RoutedEventArgs e)
         {
-            this.vm.VM_Timer += 1;
+            this.vm.VM_Timer += 0.1;
         }
 
         private void Speed_Changed_Click(object sender, RoutedEventArgs e)
         {
-            //vm.VM_Speed = Convert.ToDouble(e.Source.GetType().GetProperty("Speed").GetValue(e.Source, null));
+            ListBoxItem Speed_Box = ((sender as ListBox).SelectedItem as ListBoxItem);
+            this.vm.VM_Speed = Convert.ToDouble(Speed_Box.Content.ToString());
         }
 
         private void Drag_Started_Click(object sender, RoutedEventArgs e)
---
+++ plugins/AnomalyDetector.cs
@@ -0,0 +1,69 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+*/
+//namespace FlightSimulator.plugins
+//{
+    //class AnomalyDetector
+   // {
+        /*
+        [DllImport(".dll")]
+        public static extern IntPtr Create(int x);
+        [DllImport("AnomalyDetector.dll")]
+        public static extern int AnomalyDetectorAdd(IntPtr s, int y);
+        static void Main(string[] args)
+        {
+            IntPtr a = Create(5);
+            Console.WriteLine(AnomalyDetectorAdd(a, 11));
+            Console.WriteLine("Hello");
+        }
+        */
+        /*
+        static class NativeMethods
+        {
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr LoadLibrary(string dllToLoad);
+
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+            [DllImport("kernel32.dll")]
+            public static extern bool FreeLibrary(IntPtr hModule);
+        }
+
+        class Program
+        {
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            private delegate int MultiplyByTen(int numberToMultiply);
+
+            static void Main(string[] args)
+            {
+                IntPtr pDll = NativeMethods.LoadLibrary(@"PathToYourDll.DLL");
+                //oh dear, error handling here
+                //if (pDll == IntPtr.Zero)
+
+                IntPtr pAddressOfLearnNormal = NativeMethods.GetProcAddress(pDll, "learnNormal");
+                //oh dear, error handling here
+                //if(pAddressOfFunctionToCall == IntPtr.Zero)
+                IntPtr pAddressOfDetect = NativeMethods.GetProcAddress(pDll, "detect");
+
+                learnNormal ln = (learnNormal)Marshal.GetDelegateForFunctionPointer
+                    (pAddressOfLearnNormal,typeof(learnNormal));
+                detect d = (detect)Marshal.GetDelegateForFunctionPointer
+                    (pAddressOfLearnNormal, typeof(detect));
+
+                int theResult = multiplyByTen(10);
+
+                bool result = NativeMethods.FreeLibrary(pDll);
+                //remaining code here
+
+                Console.WriteLine(theResult);
+            }
+        }
+    }
+}
+*/
