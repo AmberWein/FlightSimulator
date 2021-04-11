6e3e20e7a4e4548f66fa47ddf27c51cc75522341
Nicole Sharabany <nicolesharabani7@gmail.com>
2021-04-11 08:22:26 +0300

    changed compass labels

--- Views/DashboardView.xaml.cs
+++ Views/DashboardView.xaml.cs
@@ -32,5 +32,38 @@ namespace FlightSimulator.Views
         {
             this.vm = dashVM;
         }
+
+        private void OrientaitionGauge_LabelCreated(Object sender, LabelCreatedEventArgs args)
+        {
+            switch ((string)args.LabelText)
+            {
+                case "0":
+                    args.LabelText="N";
+                    break;
+                case "45":
+                    args.LabelText="NE";
+                    break;
+                case "90":
+                    args.LabelText="E";
+                    break;
+                case "135":
+                    args.LabelText="SE";
+                    break;
+                case "180":
+                    args.LabelText="S";
+                    break;
+                case "225":
+                    args.LabelText = "SW";
+                    break;
+                case "270":
+                    args.LabelText="W";
+                    break;
+                case "315":
+                    args.LabelText="NW";
+                    break;
+
+
+            }
+        }
     }
 }
