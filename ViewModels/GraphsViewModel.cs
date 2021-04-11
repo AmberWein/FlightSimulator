52c8fe92b82afb73d1b4f8b9905911ae012a00a6
Avia <wolf.avia@gmail.com>
2021-04-11 18:18:22 +0300

    add graph view and model view

---
+++ ViewModels/GraphsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using FlightSimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.Models;
+using System.ComponentModel;
+
+
+
+
+namespace FlightSimulator.ViewModels
+{
+    public class GraphsViewModel : INotifyPropertyChanged
+    {
+        private IFlightSimulatorModel model; // this is a try to hold only one model for all controllers!
+        public event PropertyChangedEventHandler PropertyChanged;
+        public GraphsViewModel(IFlightSimulatorModel m)
+        {
+            this.model = m;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+
+                // NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+
+
+
+        public void NotifyPropertyChanged(String propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
+
